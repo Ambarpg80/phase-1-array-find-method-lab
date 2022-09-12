cdde694414e033b0e76eb40d321183b509beea61
Ambar <Ambarpg80@gmail.com>
2022-09-12 17:48:57 -0400

    commit tree a5cac6560e9f8099898a4eefbe5a0ba387a3a772

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+/*
+function isWin(array){
+    console.log(array.find(result => result === "W"))
+    }
+isWin(record);*/
+
+function superbowlWin(win){
+   const obj = win.find(item => item.result ==="W");
+   if (obj){
+    return obj.year
+   }
+}
+superbowlWin()
